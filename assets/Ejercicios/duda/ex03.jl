239ebb763dec015c44bcb056f9c9833f0065e3a5
marcoherrera-s <80595478+marcoherrera-s@users.noreply.github.com>
2024-01-25 02:35:26 +0000

    Deploying to gh-pages from  @ 15da5720e51b3fc65f5ead8216144057d0abdb0b 🚀

---
+++ assets/Ejercicios/duda/ex03.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+sol = solve(prob, ESDIRK547L2SA2(), maxiters = 1e7)
+sol[1:10]
--- assets/Ejercicios/duda/ex51.jl
+++
@@ -1,3 +0,0 @@
-# This file was generated, do not modify it. # hide
-sol = solve(prob, ESDIRK547L2SA2())
-sol[10]
