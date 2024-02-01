0d862e63217766cf424868174ce322e60e15ab99
marcoherrera-s <80595478+marcoherrera-s@users.noreply.github.com>
2024-02-01 08:11:39 +0000

    Deploying to gh-pages from  @ 3eedf0c21b470bf67f98441922919499baf1240e 🚀

--- assets/Ejercicios/duda/ex00006.jl
+++ assets/Ejercicios/duda/ex00006.jl
@@ -1,3 +1,3 @@
 # This file was generated, do not modify it. # hide
-sol_CR_aprox= solve(prob_CR_aprox, KenCarp47(), maxiters=1e7)
+sol_CR_aprox= solve(prob_CR_aprox, KenCarp47(), maxiters=1e6)
 sol_CR_aprox[1:5]
