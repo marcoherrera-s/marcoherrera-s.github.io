221e4bdd2a858523d6a5a886d99a5cd9c9dae2ec
marcoherrera-s <80595478+marcoherrera-s@users.noreply.github.com>
2025-02-28 07:56:39 +0000

    Deploying to gh-pages from  @ 524be96486bda8afc9833d5ec9c0abed0eac1dec 🚀

--- dark-mode.js
+++ dark-mode.js
@@ -1,27 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const toggleSwitch = document.querySelector('.toggle-switch .checkbox');
-  const darkModeStyle = document.getElementById('dark-mode-style');
-  const currentTheme = localStorage.getItem('theme');
-
-  if (currentTheme === 'dark') {
-      document.body.classList.add('dark-mode');
-      darkModeStyle.disabled = false;
-      if (toggleSwitch) {
-          toggleSwitch.checked = true;
-      }
-  }
-
-  if (toggleSwitch) {
-      toggleSwitch.addEventListener('change', function () {
-          if (this.checked) {
-              document.body.classList.add('dark-mode');
-              darkModeStyle.disabled = false;
-              localStorage.setItem('theme', 'dark');
-          } else {
-              document.body.classList.remove('dark-mode');
-              darkModeStyle.disabled = true;
-              localStorage.setItem('theme', 'light');
-          }
-      });
-  }
-});
+    const toggleSwitch = document.querySelector('.toggle-switch .checkbox');
+    const darkModeStyle = document.getElementById('dark-mode-style');
+    const highlightLight = document.getElementById('highlight-style-light');
+    const highlightDark = document.getElementById('highlight-style-dark');
+    const currentTheme = localStorage.getItem('theme');
+  
+    function enableDarkMode() {
+        document.body.classList.add('dark-mode');
+        darkModeStyle.disabled = false;
+        highlightLight.disabled = true;
+        highlightDark.disabled = false;
+        localStorage.setItem('theme', 'dark');
+    }
+  
+    function disableDarkMode() {
+        document.body.classList.remove('dark-mode');
+        darkModeStyle.disabled = true;
+        highlightLight.disabled = false;
+        highlightDark.disabled = true;
+        localStorage.setItem('theme', 'light');
+    }
+  
+    // Aplicar el tema guardado en localStorage
+    if (currentTheme === 'dark') {
+        enableDarkMode();
+        if (toggleSwitch) toggleSwitch.checked = true;
+    }
+  
+    if (toggleSwitch) {
+        toggleSwitch.addEventListener('change', function () {
+            if (this.checked) {
+                enableDarkMode();
+            } else {
+                disableDarkMode();
+            }
+        });
+    }
+  });
+  
